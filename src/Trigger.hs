03cc27ba748189c49e1e500d80aced2b9d1182a9
Rhys Keepence <rhys@rhyskeepence.com>
2017-12-07 15:00:00 +0000

    extract module to register the watcher

--- src/Trigger.hs
+++ src/Trigger.hs
@@ -1,17 +1,12 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module Trigger
   ( run
   ) where
 
-import           Data.String    (fromString)
 import           Parser
 import           Protolude
-import           System.Directory
-import           System.FilePath
-import qualified System.Process as P
-import qualified Twitch         as T
-import qualified System.FSNotify as FS
+import qualified System.FSNotify  as FS
+import qualified System.Process   as P
+import           Watcher
 
 run :: [Config] -> IO ()
 run configs = do
@@ -21,21 +16,7 @@ run configs = do
   mapM_ FS.stopManager managers
 
 runConfig :: Config -> IO FS.WatchManager
-runConfig config = do
-  root <- getBaseDirectory config
-  putStrLn root
-  c <- optionsToConfig root config
-  T.runWithConfig root c $ configToDep config
-
-configToDep :: Config -> T.Dep
-configToDep config =
-  mapM_ (fileGlobToDep config) (_files config)
-
-fileGlobToDep :: Config -> Text -> T.Dep
-fileGlobToDep config fileGlob =
-  T.addModify
-    (handleFileChange config)
-    (fromString $ toS fileGlob)
+runConfig config = watch config (handleFileChange config)
 
 handleFileChange :: Config -> FilePath -> IO ()
 handleFileChange config files = do
@@ -44,55 +25,7 @@ handleFileChange config files = do
   restartProcesses (_run config)
 
 runTasks :: Maybe [Text] -> IO ()
-runTasks tasks =
-  mapM_ (P.system . toS) (concat tasks)
+runTasks tasks = mapM_ (P.system . toS) (concat tasks)
 
 restartProcesses :: Maybe [Text] -> IO ()
-restartProcesses processes =
-  mapM_ (P.system . toS) (concat processes)
-
-
-getBaseDirectory :: Config -> IO FilePath
-getBaseDirectory Config {..} = do
-  currentDir <- getCurrentDirectory
-  makeAbsolute $ currentDir </> toS _dir
-
-optionsToConfig :: FilePath -> Config -> IO T.Config
-optionsToConfig baseDirectory Config {..} = do
-  subDirs <- findAllDirs baseDirectory
-  let dirsToWatch = baseDirectory : subDirs
-
-  let watchConfig = FS.WatchConfig
-        { FS.confDebounce     = FS.Debounce $ fromRational 200000
-        , FS.confPollInterval = 10^(6 :: Int) -- 1 second
-        , FS.confUsePolling   = False
-        }
-
-  let config = T.Config
-        { logger      = print
-        , dirs        = dirsToWatch
-        , watchConfig = watchConfig
-        }
-  return config
-
-findAllDirs :: FilePath -> IO [FilePath]
-findAllDirs path = do
-  dirs <- findImmediateDirs path
-  print dirs
-  nestedDirs <- mapM findAllDirs dirs
-  print nestedDirs
-  return (dirs ++ concat nestedDirs)
-
-findImmediateDirs :: FilePath -> IO [FilePath]
-findImmediateDirs = getDirectoryContentsPath >=> filterM doesDirectoryExist >=> canonicalize
-  where
-    canonicalize :: [FilePath] -> IO [FilePath]
-    canonicalize = mapM canonicalizeDirPath
-
-
-getDirectoryContentsPath :: FilePath -> IO [FilePath]
-getDirectoryContentsPath path =
-  map (path </>) . filter (`notElem` [".", ".."]) <$> getDirectoryContents path
-
-canonicalizeDirPath :: FilePath -> IO FilePath
-canonicalizeDirPath path = addTrailingPathSeparator <$> canonicalizePath path
+restartProcesses processes = mapM_ (P.system . toS) (concat processes)
---
+++ src/Watcher.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Watcher
+  ( watch
+  ) where
+
+import           Data.String      (fromString)
+import           Parser
+import           Protolude
+import           System.Directory
+import           System.FilePath
+import qualified System.FSNotify  as FS
+import qualified Twitch           as T
+
+type Handler = FilePath -> IO ()
+
+watch :: Config -> Handler -> IO FS.WatchManager
+watch config handler = do
+  baseDirectory <- getBaseDirectory config
+  directoriesToWatch <- getDirectoriesToWatch baseDirectory
+  T.runWithConfig baseDirectory (twitchConfig directoriesToWatch) $ configToDep config handler
+
+configToDep :: Config -> Handler -> T.Dep
+configToDep config handler = mapM_ (fileGlobToDep handler) (_files config)
+
+fileGlobToDep :: Handler -> Text -> T.Dep
+fileGlobToDep handler fileGlob = T.addModify handler (fromString $ toS fileGlob)
+
+watchConfig :: FS.WatchConfig
+watchConfig =
+  FS.WatchConfig
+  {FS.confDebounce = FS.Debounce $ fromRational 200000, FS.confPollInterval = 0, FS.confUsePolling = False}
+
+twitchConfig :: [FilePath] -> T.Config
+twitchConfig dirsToWatch = T.Config {logger = const $ return (), dirs = dirsToWatch, watchConfig = watchConfig}
+
+getBaseDirectory :: Config -> IO FilePath
+getBaseDirectory Config {..} = do
+  currentDir <- getCurrentDirectory
+  makeAbsolute $ currentDir </> toS _dir
+
+getDirectoriesToWatch :: FilePath -> IO [FilePath]
+getDirectoriesToWatch baseDirectory = do
+  subDirs <- findAllDirs baseDirectory
+  return $ baseDirectory : subDirs
+
+findAllDirs :: FilePath -> IO [FilePath]
+findAllDirs path = do
+  dirs <- findImmediateDirs path
+  nestedDirs <- mapM findAllDirs dirs
+  return (dirs ++ concat nestedDirs)
+
+findImmediateDirs :: FilePath -> IO [FilePath]
+findImmediateDirs = getDirectoryContentsPath >=> filterM doesDirectoryExist >=> canonicalize
+  where
+    canonicalize :: [FilePath] -> IO [FilePath]
+    canonicalize = mapM canonicalizeDirPath
+
+getDirectoryContentsPath :: FilePath -> IO [FilePath]
+getDirectoryContentsPath path = map (path </>) . filter (`notElem` [".", ".."]) <$> getDirectoryContents path
+
+canonicalizeDirPath :: FilePath -> IO FilePath
+canonicalizeDirPath path = addTrailingPathSeparator <$> canonicalizePath path
